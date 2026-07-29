a0de7a0348abed1bd0b90eed394755745ea2dce0
songdone <songdone@qq.com>
2026-07-29 19:22:01 +0800

    docs: pull published image during upgrades

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,6 +2,6 @@
 set -eu
 cd "$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
 ./scripts/backup.sh
-docker compose build --pull
+docker compose pull
 docker compose up -d --remove-orphans
 docker compose ps
